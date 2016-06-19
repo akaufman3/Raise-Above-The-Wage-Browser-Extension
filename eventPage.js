0ec1d102563d3ae7f097371c48ca6743dc5c3f4f
Amelia Kaufman <amelia.kaufman@gmail.com>
2016-06-19 16:09:55 -0400

    refactoring of loading and cost computations

--- eventPage.js
+++ eventPage.js
@@ -1,5 +1,14 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-	if (request.action == "show") {
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+// 	if (request.action == "show") {
+// 		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+// 			chrome.pageAction.show(tabs[0].id);
+// 		});
+// 	}
+// });
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+	if (changeInfo.status == 'complete') {
+	  	alert("completed")
 		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 			chrome.pageAction.show(tabs[0].id);
 		});
--- raiseAgainst_cs.js
+++ raiseAgainst_cs.js
@@ -1,3 +1,7 @@
+// var twoDecimal = new RegExp(/\.[0-9]{2}/);
+// var commaCheck = new RegExp(/[0-9,]+$/g);
+// var commaCheck = new RegExp(/[,]/g);
+
 var costAdjustments = {
 	whiteMale: 0,
 	whiteWoman: 1.28,
@@ -6,95 +10,84 @@ var costAdjustments = {
 	asianAmericanWoman: 1.11
 };
 
-var currentCostAdjustments = 0;
-
 var i;
-var costs = [];
-var twoDecimal = new RegExp(/\.[0-9]{2}/);
-// var commaCheck = new RegExp(/[0-9,]+$/g);
-var commaCheck = new RegExp(/[,]/g);
+var whiteMaleCosts = [];
+var whiteWomanCosts = [];
+var africanAmericanWomanCosts = [];
+var hispanicWomanCosts = [];
+var asianAmericanWomanCosts = [];
+
+
+var findDollars =/\$\d+[0-9\.,]+?(?=\D\D)\b(?! )/g;
+while (m = findDollars.exec(document.body.innerText)) { 
+    var currentCost = m[0].replace(/\$/g, "");
+	whiteMaleCosts.push(currentCost);
+	
+	var whiteWomanCost = currentCost * costAdjustments.whiteWoman;
+	whiteWomanCosts.push(whiteWomanCost);
+
+	var africanAmericanWomanCost = currentCost * costAdjustments.africanAmericanWoman;
+	africanAmericanWomanCosts.push(africanAmericanWomanCost);
+
+	var hispanicWomanCost = currentCost * costAdjustments.hispanicWoman;
+	hispanicWomanCosts.push(hispanicWomanCost);	
+
+	var asianAmericanWomanCost = currentCost * costAdjustments.asianAmericanWoman;
+	asianAmericanWomanCosts.push(asianAmericanWomanCost);	
+}
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 
 	if(request.action == "whiteMale") {
 		var i = 0;
+		var maleCost = '$' + Math.abs(parseFloat(whiteMaleCosts[i])).toFixed(2);
 		document.body.innerHTML = document.body.innerHTML.replace(/\$\d+[0-9\.,]+?(?=\D\D)\b(?! )/g, function(match){
-			if (currentCostAdjustments !== 0) {
-				var maleCost = '$' + Math.abs(parseFloat(costs[i])).toFixed(2);
-			} else {
-				var currentCost = match.replace(/\$/g, "");
-				costs.push(currentCost);
-				var maleCost = '$' + Math.abs(parseFloat(currentCost)).toFixed(2);
-			}
+			console.log(match, "match")
+			var newValue = '$' + Math.abs(parseFloat(whiteMaleCosts[i])).toFixed(2);
+			console.log(newValue, "newValue")
 			i++;
-			return maleCost;
+			return match.replace(match, newValue);
 		});
 	} else if(request.action == "whiteWoman") {
-			var i = 0;
-			document.body.innerHTML = document.body.innerHTML.replace(/\$\d+[0-9\.,]+?(?=\D\D)\b(?! )/g, function(match){
-				if (currentCostAdjustments !== 0) {
-					var womenCostDiff = costs[i] * costAdjustments.whiteWoman;
-					var womenCost = '$' + Math.abs(parseFloat(womenCostDiff)).toFixed(2);
-				} else {
-					var currentCost = match.replace(/\$/g, "");
-					costs.push(currentCost);
-					var womenCostDiff = currentCost * costAdjustments.whiteWoman;
-					var womenCost = '$' +  Math.abs(parseFloat(womenCostDiff)).toFixed(2);
-				}
-				i++;
-				return womenCost;
-			});
+		var i = 0;
+		document.body.innerHTML = document.body.innerHTML.replace(/\$\d+[0-9\.,]+?(?=\D\D)\b(?! )/g, function(match){
+			var womenCost = '$' + Math.abs(parseFloat(whiteWomanCosts[i])).toFixed(2);
+			i++;
+			return womenCost;
+		});
 	} else if(request.action == "africanAmericanWoman") {
 		var i = 0;
 		document.body.innerHTML = document.body.innerHTML.replace(/\$\d+[0-9\.,]+?(?=\D\D)\b(?! )/g, function(match){
-			if (currentCostAdjustments !== 0) {
-				var womenCostDiff = costs[i] * costAdjustments.africanAmericanWoman;
-				var womenCost = '$' + Math.abs(parseFloat(womenCostDiff)).toFixed(2);
-			} else {
-				var currentCost = match.replace(/\$/g, "");
-				costs.push(currentCost);
-				var womenCostDiff = costs[i] * costAdjustments.africanAmericanWoman;
-				var womenCost = '$' + Math.abs(parseFloat(womenCostDiff)).toFixed(2);
-			}
+			var womenCost = '$' + Math.abs(parseFloat(africanAmericanWomanCosts[i])).toFixed(2);
 			i++;
 			return womenCost;
 		});
 	} else if(request.action == "hispanicWoman") {
 		var i = 0;
 		document.body.innerHTML = document.body.innerHTML.replace(/\$\d+[0-9\.,]+?(?=\D\D)\b(?! )/g, function(match){
-			if (currentCostAdjustments !== 0) {
-				var womenCostDiff = costs[i] * costAdjustments.hispanicWoman;
-				var womenCost = '$' + Math.abs(parseFloat(womenCostDiff)).toFixed(2);
-			} else {
-				var currentCost = match.replace(/\$/g, "");
-				costs.push(currentCost);
-				var womenCostDiff = currentCost * costAdjustments.hispanicWoman;
-				var womenCost = '$' + Math.abs(parseFloat(womenCostDiff)).toFixed(2);
-			}
+			var womenCost = '$' + Math.abs(parseFloat(hispanicWomanCosts[i])).toFixed(2);
 			i++;
 			return womenCost;
 		});
 	} else if(request.action == "asianAmericanWoman") {
 		var i = 0;
 		document.body.innerHTML = document.body.innerHTML.replace(/\$\d+[0-9\.,]+?(?=\D\D)\b(?! )/g, function(match){
-			if (currentCostAdjustments !== 0) {
-				var womenCostDiff = costs[i] * costAdjustments.asianAmericanWoman;
-				var womenCost = '$' + Math.abs(parseFloat(womenCostDiff)).toFixed(2);
-			} else {
-				var currentCost = match.replace(/\$/g, "");
-				costs.push(currentCost);
-				var womenCostDiff = currentCost * costAdjustments.asianAmericanWoman;
-				var womenCost = '$' + Math.abs(parseFloat(womenCostDiff)).toFixed(2);
-			}
+			var womenCost = '$' + Math.abs(parseFloat(asianAmericanWomanCosts[i])).toFixed(2);
 			i++;
 			return womenCost;
 		});
 	}
 
 	var requestAction = request.action;
-	currentCostAdjustments = costAdjustments[requestAction];
 });
 
+console.log(whiteMaleCosts, "whiteMalecosts");
+console.log(whiteWomanCosts, "whiteWomanCosts");
+console.log(africanAmericanWomanCosts, "africanAmericanWomanCosts");
+console.log(hispanicWomanCosts, "hispanicWomanCosts");
+console.log(asianAmericanWomanCosts, "asianAmericanWomanCosts");
+
+
 chrome.runtime.sendMessage({
-	action: "show"
+	changeInfo: "completed"
 });
