1e4320ff68aabe92e81e233220627616af9f0b91
Molly Kraus <mollyjkraus@gmail.com>
2016-07-11 14:20:38 -0400

    finalized static layout

--- popup.js
+++ popup.js
@@ -9,17 +9,17 @@ $(function() {
 			chrome.tabs.sendMessage(tabs[0].id, {action: "whiteWoman"});
 		});
 	});
-	$('#africanAmericanWoman').click(function() {
+	$('#africanAmericanWoman').click(function() {	
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
 			chrome.tabs.sendMessage(tabs[0].id, {action: "africanAmericanWoman"});
 		});
 	});
-	$('#hispanicWoman').click(function() {
+	$('#hispanicWoman').click(function() {	
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
 			chrome.tabs.sendMessage(tabs[0].id, {action: "hispanicWoman"});
 		});
 	});
-	$('#asianAmericanWoman').click(function() {
+	$('#asianAmericanWoman').click(function() {	
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
 			chrome.tabs.sendMessage(tabs[0].id, {action: "asianAmericanWoman"});
 		});
